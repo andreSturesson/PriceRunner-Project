953cb05da360f666509a56eb955d3402f3d4abf7
André Sturesson <andre.m.sturesson@gmail.com>
2024-03-13 16:27:38 +0100

    Created backend testing project

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b518d5f9bef315c0e4f9f3b0acca072e1599156e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dfd235707a49856f5851d525b96b369d7457ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
