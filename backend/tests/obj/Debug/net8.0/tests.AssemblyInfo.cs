29661bee2f9f134f8e83cdac22e1cc3e0a47a87b
André Sturesson <andre.m.sturesson@gmail.com>
2024-03-15 15:52:41 +0100

    Created interface

--- backend/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ backend/tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c8911ef215a5618828d2cdbe56a6e35f2c6d5f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf86f889783a40a18b5a624950457d2c1b359c04")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
