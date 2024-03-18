15bb783a8100ee863fac9f01c584e1a911b7fe47
Marcus Palm <marcus.palm@se.experis.com>
2024-03-18 15:27:13 +0100

    working on the sidebar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,16 @@ import ProductsPage from "./Pages/ProductsPage";
 import ErrorPage from "./Pages/ErrorPage";
 import TopNavigation from "./Components/TopNavigation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SideBar from "./Components/SideBar";
 
 function App() {
   return (
     <>
+      <SideBar />
       <div>Hello World</div>
       {/*<MainPage />*/}
       <TopNavigation />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
--- frontend/src/Components/SideBar.jsx
+++ frontend/src/Components/SideBar.jsx
@@ -1,3 +1,39 @@
-function SideBar() {}
+import "./SideBar.css";
+import { Group } from "@mantine/core";
+function SideBar() {
+  const isLoggedIn = true; //get this from login condition
+
+  let isCollapsed = true;
+
+  function toggleSidebar() {
+    if (isCollapsed) {
+      document.getElementById("colapsableSidebar").style.width = "350px";
+      isCollapsed = false;
+      console.log("expanding");
+    } else {
+      document.getElementById("colapsableSidebar").style.width = "85px";
+      isCollapsed = true;
+      console.log("reduceing");
+    }
+  }
+
+  return (
+    <div id="colapsableSidebar" className="sideBar">
+      <Group gap="xs">
+        <div>
+          <button className="sidebar-button">Home</button>
+          {isLoggedIn && (
+            <button className="sidebar-button" onClick={toggleSidebar}>
+              wishList
+            </button>
+          )}
+        </div>
+        {isLoggedIn && (
+          <div className="whishlistTest">wishlist will go here</div>
+        )}
+      </Group>
+    </div>
+  );
+}
 
 export default SideBar;
