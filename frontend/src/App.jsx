54328c274ef5e0c702e081156f5cd7ed6b3fac0d
Marcus Palm <marcus.palm@se.experis.com>
2024-03-18 08:56:05 +0100

    Added Routing for MianPage, ProductsPage and ProductPage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import MainPage from "./Pages/MainPage";
+import ProductPage from "./Pages/ProductPage";
+import ProductsPage from "./Pages/ProductsPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div>Hello World</div>
-      <MainPage />
+      {/*<MainPage />*/}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/product/:productId" element={<ProductPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
