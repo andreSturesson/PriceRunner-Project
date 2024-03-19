3872745a98a00ee21d2c10a099ac2624c0b2683f
André Sturesson <andre.m.sturesson@gmail.com>
2024-03-19 13:15:36 +0100

    Added filtering, pagination and get by category

--- backend/backend/Repository/Interfaces/IProductRepository.cs
+++ backend/backend/Repository/Interfaces/IProductRepository.cs
@@ -6,6 +6,6 @@ namespace backend.Repository.Interfaces
   public interface IProductRepository
   {
     Task<Product> GetProductById(int id);
-    Task<IEnumerable<Product>> GetProducts();
+    Task<IEnumerable<Product>> GetProducts(string query = null, int categoryId = 0, int page = 1, int limit = 10);
   }
 }
--- backend/backend/View/Endpoints/ProductsEndpoints.cs
+++ backend/backend/View/Endpoints/ProductsEndpoints.cs
@@ -18,12 +18,12 @@ namespace backend.View.Endpoints
 
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public static async Task<IResult> GetProducts(IProductRepository productRepository)
+    public static async Task<IResult> GetProducts(IProductRepository productRepository, string query = null, int categoryId = 0, int page = 1, int limit = 10)
     {
       try
       {
-        var products = await productRepository.GetProducts();
-        if (products == null)
+        var products = await productRepository.GetProducts(query, categoryId, page, limit);
+        if (products == null || !products.Any())
         {
           return Results.NotFound(new Error(Status.NotFound, "No products found"));
         }
@@ -34,6 +34,7 @@ namespace backend.View.Endpoints
         return Results.Conflict(new Error(Status.InternalServerError, ex.Message));
       }
     }
+
 
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
