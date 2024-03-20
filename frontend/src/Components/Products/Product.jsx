ac6d8a2f3f9cd95d813726600f22d58780e9f92b
André Sturesson <andre.m.sturesson@gmail.com>
2024-03-20 10:13:33 +0100

    Added more on product

--- frontend/src/Components/Products/Product.jsx
+++ frontend/src/Components/Products/Product.jsx
@@ -2,16 +2,10 @@ import { Card, Center, Image, Space, Text } from "@mantine/core";
 import PropTypes from "prop-types";
 
 function Product({ product }) {
+  console.log(product);
   return (
     <>
-      <Card
-        shadow="sm"
-        padding="xl"
-        component="a"
-        href={product.productUrl}
-        target="_blank"
-        withBorder
-      >
+      <Card shadow="sm" padding="xl" component="a" target="_blank" withBorder>
         <Card.Section>
           <Center>
             <Image src={product.imageUrl} h={400} w={350} alt="No way!" />
@@ -19,10 +13,23 @@ function Product({ product }) {
         </Card.Section>
 
         <Text fw={500} size="lg" mt="md">
-          {product.title}
+          <a
+            style={{ textDecoration: "none", color: "inherit" }}
+            href={product.productUrl}
+            target="_blank"
+          >
+            {product.title}
+          </a>
         </Text>
 
         <Text size="sm">Price: ${product.price.toFixed(2)}</Text>
+
+        <Text size="sm">
+          Stars: {Array(parseInt(product.stars)).fill("⭐️").join("")}
+        </Text>
+        <Text size="xs" c="dimmed">
+          {product.category.name}
+        </Text>
       </Card>
       <Space h="md" mt="xl" />
     </>
